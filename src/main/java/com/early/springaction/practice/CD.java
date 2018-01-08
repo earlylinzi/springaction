b37057a6dc44726a5c44cb0acc5fb81712b06a43
80002523 <80002523@sf-express.com>
2018-01-08 19:31:27 +0800

    [add some code ]

--- src/main/java/com/early/springaction/practice/CD.java
+++ src/main/java/com/early/springaction/practice/CD.java
@@ -2,6 +2,9 @@ package com.early.springaction.practice;
 
 import com.early.springaction.interfaces.CompactDist;
 
+/**
+ * CD的具体实现类（实现了CompactDist接口的类）
+ */
 public class CD implements CompactDist {
 
     public void play() {
--- src/main/java/com/early/springaction/practice/CDPlayer.java
+++ src/main/java/com/early/springaction/practice/CDPlayer.java
@@ -3,6 +3,9 @@ package com.early.springaction.practice;
 import com.early.springaction.interfaces.CompactDist;
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * CD播放的类
+ */
 public class CDPlayer {
 
     private CompactDist cd;
